69b2cfe14b0e577e6238ded6e91a706b9af92b0e
Merima Dedic <merimaxdedic@gmail.com>
2023-11-07 18:10:02 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  let mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+
+  function wrapAdjective(symbol) {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    };
+  }
+  
+  console.log(wrapAdjective('*')('a hard worker'));
+  console.log(wrapAdjective('||')('a hard worker'));
+
+ 
